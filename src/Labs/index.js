9f1d4db232b6b66547eff2abf36360748edf4a35
chenxuan929 <xu.chenxu@northeastern.edu>
2023-10-19 19:38:53 -0400

    1

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,19 +1,42 @@
 import { Route, Routes, Link, useLocation, Navigate } from "react-router-dom";
 import Assignment3 from "./a3";
-import { Link } from "react-router-dom";
-import Nav from "../Nav";
+import Kanbas from "../Kanbas";
 
 
 function Labs() {
   const { pathname } = useLocation();
 
-    return (
-      <div>
-        <Nav />
-        {/* <Link to="/hello">Hello</Link> |<Link to="/Labs/a3">A3</Link> |
-        <Link to="/Kanbas">Kanbas</Link> */}
-        <Assignment3 />
+  return (
+    <div className="container">
+      <h1>Labs</h1>
+
+      <div className="nav nav-pills">
+        <Link
+          to="/Labs/a3"
+          className={`nav-link ${pathname.includes('a3') ? "active" : ""
+            }`}
+        >
+          Assignment 3
+        </Link>
+
+        <Link
+          to="/Kanbas/Dashboard"
+          className={`nav-link ${pathname.includes('Kanbas') ? "active" : ""
+            }`}
+        >
+          Kanbas
+        </Link>
+
+
       </div>
-    );
-  }
-  export default Labs;
+      <Routes>
+        <Route path="/" element={<Navigate to="Kanbas" />} />
+        <Route path="a3/*" element={<Assignment3 />} />
+
+
+      </Routes>
+      
+    </div >
+  );
+}
+export default Labs;
