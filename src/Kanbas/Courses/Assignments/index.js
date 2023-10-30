6f083b14975e129b38482bce57b147ad09eb1200
chenxuan929 <xu.chenxu@northeastern.edu>
2023-10-29 23:40:21 -0400

    .

--- src/Kanbas/Courses/Assignments/index.js
+++ src/Kanbas/Courses/Assignments/index.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import db from "../../Database";
-import { useState } from "react";
 import { BiCheckCircle, BiDotsVerticalRounded } from "react-icons/bi";
-
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addAssignment,
+  deleteAssignment,
+  updateAssignment,
+  setAssignment,
+} from "./AssignmentsReducer";
 
 function Assignments() {
-
   const { courseId } = useParams();
+  /*
   const assignments = db.assignments;
+  */
+
+
+  const assignments = useSelector((state) => state.AssignmentsReducer.assignments);
+  const assignment = useSelector((state) => state.AssignmentsReducer.assignment);
+  const dispatch = useDispatch();
+
   const courseAssignments = assignments.filter(
     (assignment) => assignment.course === courseId);
+
 
   // Create a state to track the visibility of each module's content
   const [assignmentVisibility, setAssignmentVisibility] = useState({});
@@ -38,11 +51,39 @@ function Assignments() {
         <li>
           <button type="button" class="btn btn-secondary mr-1" style={{ backgroundColor: 'rgb(237, 235, 235)', color: 'grey', marginLeft: '5px' }}>Group</button>
         </li>
+        
+
         <li>
-          <button type="button" class="btn btn-danger mr-1" style={{ marginLeft: '5px' }}>
-            <i aria-hidden="true"></i>+ Assignment
+          <input
+            value={assignment._id}
+            onChange={(e) =>
+              dispatch(setAssignment({ ...assignment, _id: e.target.value }))
+            } />
+        </li>
+        <li>
+          <textarea
+            style={{ marginLeft: '5px', marginRight: '5px' }}
+            value={assignment.title}
+            onChange={(e) => dispatch(setAssignment({ ...assignment, title: e.target.value }))
+            } />
+        </li>
+        <li>
+          <button
+            onClick={() => dispatch(addAssignment({ ...assignment, course: courseId }))}
+            type="button" class="btn btn-danger mr-1" style={{ marginLeft: '5px' }}>
+            <i aria-hidden="true"></i>Add
           </button>
         </li>
+
+        <li>
+          <button
+            onClick={() => dispatch(updateAssignment(assignment))}
+            type="button" class="btn btn-danger mr-1" style={{ marginLeft: '5px', marginRight: '5px' }}>
+            <i aria-hidden="true"></i>Update
+          </button>
+        </li>
+
+
         <li>
           <button type="button" class="btn btn-secondary mr-1" style={{ backgroundColor: 'rgb(237, 235, 235)', color: 'grey', marginLeft: '5px' }}>
             <BiDotsVerticalRounded className="wd-icon" />
@@ -86,10 +127,22 @@ function Assignments() {
                 </Link>
               </span>
               <span>
-                <BiCheckCircle className="wd-icon" style={{ color: 'green' }}/>
+                <BiCheckCircle className="wd-icon" style={{ color: 'green' }} />
                 <BiDotsVerticalRounded className="wd-icon" />
+                <button
+                  class="btn btn-secondary mr-1" style={{ backgroundColor: 'rgb(237, 235, 235)', color: 'grey', marginRight: '5px' }}
+                  onClick={() => dispatch(setAssignment(assignment))}>
+                  Edit
+                </button>
+                <button
+                  class="btn btn-danger mr-1"
+                  onClick={() => dispatch(deleteAssignment(assignment._id))}>
+                  Delete
+                </button>
               </span>
             </h3>
+
+
             <p style={{ marginLeft: '30px' }}>{assignment._id} {assignment.title} {assignment.course}</p>
           </li>
         ))}
--- src/Kanbas/store/index.js
+++ src/Kanbas/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modulesReducer from "../Courses/Modules/modulesReducer";
+import AssignmentsReducer from "../Courses/Assignments/AssignmentsReducer";
 
 
 const store = configureStore({
   reducer: {
-    modulesReducer
+    modulesReducer,
+    AssignmentsReducer
   }
 });
 
